512a3b878901da5a8fbfd4893aa87ece4b39e46e
Jack24658735 <61014449+Jack24658735@users.noreply.github.com>
2020-03-14 22:05:56 +0800

    Update 12_1_charRange.py

--- self_check/HW12/12_1_charRange.py
+++ self_check/HW12/12_1_charRange.py
@@ -10,13 +10,3 @@ def CharRange(start, end):
             yield chr(ord(start) - i)
             #ans += chr(ord(start) - i)
         #return ans
-'''
-# For judging purpose
-instantiate_generator1 = input()
-exec(instantiate_generator1)
-instantiate_generator2 = input()
-exec(instantiate_generator2)
-for i in range(4):
-    test_code = input()
-    exec(test_code)
-'''
